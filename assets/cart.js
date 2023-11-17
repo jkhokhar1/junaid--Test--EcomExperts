0e10eacfe798e7fd594aa25a9cf7ce4e901cd48a
Junaid <junaid.khokhar@gmail.com>
2023-11-18 00:28:58 +0500

    update

--- assets/cart.js
+++ assets/cart.js
@@ -1,34 +1,43 @@
 class CartRemoveButton extends HTMLElement {
   constructor() {
-    super();
+    super()
     this.addEventListener('click', (event) => {
-      event.preventDefault();
-      const cartItems = this.closest('cart-items') || this.closest('cart-drawer-items');
-      cartItems.updateQuantity(this.dataset.index, 0);
-    });
+      event.preventDefault()
+      const cartItems =
+        this.closest('cart-items') || this.closest('cart-drawer-items')
+      cartItems.updateQuantity(this.dataset.index, 0)
+    })
   }
 }
 
-customElements.define('cart-remove-button', CartRemoveButton);
+customElements.define('cart-remove-button', CartRemoveButton)
 
 class CartItems extends HTMLElement {
   constructor() {
-    super();
+    super()
 
-    this.lineItemStatusElement = document.getElementById('shopping-cart-line-item-status') || document.getElementById('CartDrawer-LineItemStatus');
+    this.lineItemStatusElement =
+      document.getElementById('shopping-cart-line-item-status') ||
+      document.getElementById('CartDrawer-LineItemStatus')
 
-    this.currentItemCount = Array.from(this.querySelectorAll('[name="updates[]"]'))
-      .reduce((total, quantityInput) => total + parseInt(quantityInput.value), 0);
+    this.currentItemCount = Array.from(
+      this.querySelectorAll('[name="updates[]"]')
+    ).reduce((total, quantityInput) => total + parseInt(quantityInput.value), 0)
 
     this.debouncedOnChange = debounce((event) => {
-      this.onChange(event);
-    }, 300);
+      this.onChange(event)
+    }, 300)
 
-    this.addEventListener('change', this.debouncedOnChange.bind(this));
+    this.addEventListener('change', this.debouncedOnChange.bind(this))
   }
 
   onChange(event) {
-    this.updateQuantity(event.target.dataset.index, event.target.value, document.activeElement.getAttribute('name'));
+    console.log(event.target.value, 'event.target.value')
+    this.updateQuantity(
+      event.target.dataset.index,
+      event.target.value,
+      document.activeElement.getAttribute('name')
+    )
   }
 
   getSectionsToRender() {
@@ -36,7 +45,7 @@ class CartItems extends HTMLElement {
       {
         id: 'main-cart-items',
         section: document.getElementById('main-cart-items').dataset.id,
-        selector: '.js-contents',
+        selector: '.js-contents'
       },
       {
         id: 'cart-icon-bubble',
@@ -51,211 +60,267 @@ class CartItems extends HTMLElement {
       {
         id: 'main-cart-footer',
         section: document.getElementById('main-cart-footer').dataset.id,
-        selector: '.js-contents',
+        selector: '.js-contents'
       }
-    ];
+    ]
   }
 
   updateQuantity(line, quantity, name) {
-    this.enableLoading(line);
-    console.log(quantity);
+    this.enableLoading(line)
+    console.log(quantity)
     const body = JSON.stringify({
       line,
       quantity,
       sections: this.getSectionsToRender().map((section) => section.section),
       sections_url: window.location.pathname
-    });
+    })
 
     fetch(`${routes.cart_change_url}`, { ...fetchConfig(), ...{ body } })
       .then((response) => {
-        return response.text();
+        return response.text()
       })
       .then((state) => {
-        const parsedState = JSON.parse(state);
-        this.classList.toggle('is-empty', parsedState.item_count === 0);
-        const cartDrawerWrapper = document.querySelector('cart-drawer');
-        const cartFooter = document.getElementById('main-cart-footer');
+        const parsedState = JSON.parse(state)
+        this.classList.toggle('is-empty', parsedState.item_count === 0)
+        const cartDrawerWrapper = document.querySelector('cart-drawer')
+        const cartFooter = document.getElementById('main-cart-footer')
 
-        if (cartFooter) cartFooter.classList.toggle('is-empty', parsedState.item_count === 0);
-        if (cartDrawerWrapper) cartDrawerWrapper.classList.toggle('is-empty', parsedState.item_count === 0);
+        if (cartFooter)
+          cartFooter.classList.toggle('is-empty', parsedState.item_count === 0)
+        if (cartDrawerWrapper)
+          cartDrawerWrapper.classList.toggle(
+            'is-empty',
+            parsedState.item_count === 0
+          )
 
-        this.getSectionsToRender().forEach((section => {
+        this.getSectionsToRender().forEach((section) => {
           const elementToReplace =
-            document.getElementById(section.id).querySelector(section.selector) || document.getElementById(section.id);
-          elementToReplace.innerHTML =
-            this.getSectionInnerHTML(parsedState.sections[section.section], section.selector);
-        }));
+            document
+              .getElementById(section.id)
+              .querySelector(section.selector) ||
+            document.getElementById(section.id)
+          elementToReplace.innerHTML = this.getSectionInnerHTML(
+            parsedState.sections[section.section],
+            section.selector
+          )
+        })
 
-        this.updateLiveRegions(line, parsedState.item_count);
-        const lineItem = document.getElementById(`CartItem-${line}`) || document.getElementById(`CartDrawer-Item-${line}`);
+        this.updateLiveRegions(line, parsedState.item_count)
+        const lineItem =
+          document.getElementById(`CartItem-${line}`) ||
+          document.getElementById(`CartDrawer-Item-${line}`)
         if (lineItem && lineItem.querySelector(`[name="${name}"]`)) {
-          cartDrawerWrapper ? trapFocus(cartDrawerWrapper, lineItem.querySelector(`[name="${name}"]`)) : lineItem.querySelector(`[name="${name}"]`).focus();
+          cartDrawerWrapper
+            ? trapFocus(
+                cartDrawerWrapper,
+                lineItem.querySelector(`[name="${name}"]`)
+              )
+            : lineItem.querySelector(`[name="${name}"]`).focus()
         } else if (parsedState.item_count === 0 && cartDrawerWrapper) {
-          trapFocus(cartDrawerWrapper.querySelector('.drawer__inner-empty'), cartDrawerWrapper.querySelector('a'))
+          trapFocus(
+            cartDrawerWrapper.querySelector('.drawer__inner-empty'),
+            cartDrawerWrapper.querySelector('a')
+          )
         } else if (document.querySelector('.cart-item') && cartDrawerWrapper) {
-          trapFocus(cartDrawerWrapper, document.querySelector('.cart-item__name'))
+          trapFocus(
+            cartDrawerWrapper,
+            document.querySelector('.cart-item__name')
+          )
         }
-        this.disableLoading();
+        this.disableLoading()
         // start-ECOM ---
         // checking addon items
         // removing/changing qty of addon based on main product
         // end-ECOM ---
-        this.checkCartItems();
-      }).catch(() => {
-        this.querySelectorAll('.loading-overlay').forEach((overlay) => overlay.classList.add('hidden'));
-        const errors = document.getElementById('cart-errors') || document.getElementById('CartDrawer-CartErrors');
-        errors.textContent = window.cartStrings.error;
-        this.disableLoading();
-
-      });
-
+        this.checkCartItems()
+      })
+      .catch(() => {
+        this.querySelectorAll('.loading-overlay').forEach((overlay) =>
+          overlay.classList.add('hidden')
+        )
+        const errors =
+          document.getElementById('cart-errors') ||
+          document.getElementById('CartDrawer-CartErrors')
+        errors.textContent = window.cartStrings.error
+        this.disableLoading()
+      })
   }
 
   updateLiveRegions(line, itemCount) {
     if (this.currentItemCount === itemCount) {
-      const lineItemError = document.getElementById(`Line-item-error-${line}`) || document.getElementById(`CartDrawer-LineItemError-${line}`);
-      const quantityElement = document.getElementById(`Quantity-${line}`) || document.getElementById(`Drawer-quantity-${line}`);
-      lineItemError
-        .querySelector('.cart-item__error-text')
-        .innerHTML = window.cartStrings.quantityError.replace(
+      const lineItemError =
+        document.getElementById(`Line-item-error-${line}`) ||
+        document.getElementById(`CartDrawer-LineItemError-${line}`)
+      const quantityElement =
+        document.getElementById(`Quantity-${line}`) ||
+        document.getElementById(`Drawer-quantity-${line}`)
+      lineItemError.querySelector('.cart-item__error-text').innerHTML =
+        window.cartStrings.quantityError.replace(
           '[quantity]',
           quantityElement.value
-        );
+        )
     }
 
-    this.currentItemCount = itemCount;
-    this.lineItemStatusElement.setAttribute('aria-hidden', true);
+    this.currentItemCount = itemCount
+    this.lineItemStatusElement.setAttribute('aria-hidden', true)
 
-    const cartStatus = document.getElementById('cart-live-region-text') || document.getElementById('CartDrawer-LiveRegionText');
-    cartStatus.setAttribute('aria-hidden', false);
+    const cartStatus =
+      document.getElementById('cart-live-region-text') ||
+      document.getElementById('CartDrawer-LiveRegionText')
+    cartStatus.setAttribute('aria-hidden', false)
 
     setTimeout(() => {
-      cartStatus.setAttribute('aria-hidden', true);
-    }, 1000);
+      cartStatus.setAttribute('aria-hidden', true)
+    }, 1000)
   }
 
   getSectionInnerHTML(html, selector) {
     return new DOMParser()
       .parseFromString(html, 'text/html')
-      .querySelector(selector).innerHTML;
+      .querySelector(selector).innerHTML
   }
 
   enableLoading(line) {
-    const mainCartItems = document.getElementById('main-cart-items') || document.getElementById('CartDrawer-CartItems');
-    mainCartItems.classList.add('cart__items--disabled');
+    const mainCartItems =
+      document.getElementById('main-cart-items') ||
+      document.getElementById('CartDrawer-CartItems')
+    mainCartItems.classList.add('cart__items--disabled')
 
-    const cartItemElements = this.querySelectorAll(`#CartItem-${line} .loading-overlay`);
-    const cartDrawerItemElements = this.querySelectorAll(`#CartDrawer-Item-${line} .loading-overlay`);
+    const cartItemElements = this.querySelectorAll(
+      `#CartItem-${line} .loading-overlay`
+    )
+    const cartDrawerItemElements = this.querySelectorAll(
+      `#CartDrawer-Item-${line} .loading-overlay`
+    )
 
-    [...cartItemElements, ...cartDrawerItemElements].forEach((overlay) => overlay.classList.remove('hidden'));
+    ;[...cartItemElements, ...cartDrawerItemElements].forEach((overlay) =>
+      overlay.classList.remove('hidden')
+    )
 
-    document.activeElement.blur();
-    this.lineItemStatusElement.setAttribute('aria-hidden', false);
+    document.activeElement.blur()
+    this.lineItemStatusElement.setAttribute('aria-hidden', false)
   }
 
   disableLoading() {
-    const mainCartItems = document.getElementById('main-cart-items') || document.getElementById('CartDrawer-CartItems');
-    mainCartItems.classList.remove('cart__items--disabled');
+    const mainCartItems =
+      document.getElementById('main-cart-items') ||
+      document.getElementById('CartDrawer-CartItems')
+    mainCartItems.classList.remove('cart__items--disabled')
   }
 
-  checkCartItems(){
-    var itemsArray = [];
+  checkCartItems() {
+    var itemsArray = []
     fetch('/cart.js')
-        .then(response => {
-          response.json().then(cart => {
-            var items = cart.items;
-            items.forEach(function (item,index){
-              if(item.properties != null){
-                if(item.properties['_product_key']!= undefined){
-                  itemsArray[item.variant_id] = [];
-                  itemsArray[item.variant_id]['key'] = item.properties['_product_key'];
-                  itemsArray[item.variant_id]['line'] = index+1;
-                  itemsArray[item.variant_id]['qty'] = item.quantity;
-                }
-              }
-            });
-            for(var index in itemsArray) {
-              var checkItem = items.find(x => x.key === itemsArray[index]['key']);
-              if(checkItem == undefined){
-                this.updateQuantity(itemsArray[index]['line'], 0, document.activeElement.getAttribute('name'));
-              }else if(checkItem.quantity != itemsArray[index]['qty']){
-                this.updateQuantity(itemsArray[index]['line'], checkItem.quantity, document.activeElement.getAttribute('name'));
+      .then((response) => {
+        response.json().then((cart) => {
+          var items = cart.items
+          items.forEach(function (item, index) {
+            if (item.properties != null) {
+              if (item.properties['_product_key'] != undefined) {
+                itemsArray[item.variant_id] = []
+                itemsArray[item.variant_id]['key'] =
+                  item.properties['_product_key']
+                itemsArray[item.variant_id]['line'] = index + 1
+                itemsArray[item.variant_id]['qty'] = item.quantity
               }
             }
           })
+          for (var index in itemsArray) {
+            var checkItem = items.find(
+              (x) => x.key === itemsArray[index]['key']
+            )
+            if (checkItem == undefined) {
+              this.updateQuantity(
+                itemsArray[index]['line'],
+                0,
+                document.activeElement.getAttribute('name')
+              )
+            } else if (checkItem.quantity != itemsArray[index]['qty']) {
+              this.updateQuantity(
+                itemsArray[index]['line'],
+                checkItem.quantity,
+                document.activeElement.getAttribute('name')
+              )
+            }
+          }
         })
-        .catch(error => {
-          // handle the error
-        });
+      })
+      .catch((error) => {
+        // handle the error
+      })
   }
 }
 
-customElements.define('cart-items', CartItems);
+customElements.define('cart-items', CartItems)
 
 if (!customElements.get('cart-note')) {
-  customElements.define('cart-note', class CartNote extends HTMLElement {
-    constructor() {
-      super();
+  customElements.define(
+    'cart-note',
+    class CartNote extends HTMLElement {
+      constructor() {
+        super()
 
-      this.addEventListener('change', debounce((event) => {
-        const body = JSON.stringify({ note: event.target.value });
-        fetch(`${routes.cart_update_url}`, { ...fetchConfig(), ...{ body } });
-      }, 300))
+        this.addEventListener(
+          'change',
+          debounce((event) => {
+            const body = JSON.stringify({ note: event.target.value })
+            fetch(`${routes.cart_update_url}`, {
+              ...fetchConfig(),
+              ...{ body }
+            })
+          }, 300)
+        )
+      }
     }
-  });
-};
+  )
+}
 
 //checkCartItems();
-function checkCartItems(){
-  var labourProduct= false;
-  var frameProduct = false;
-  var itemsToRemove = [];
-  var itemsArray = [];
+function checkCartItems() {
+  var labourProduct = false
+  var frameProduct = false
+  var itemsToRemove = []
+  var itemsArray = []
 
   fetch('/cart.js')
-      .then(response => {
-        response.json().then(cart => {
-          console.log(cart);
-          var items = cart.items;
-          items.forEach(function (item,index){
-            console.log(item.properties);
+    .then((response) => {
+      response.json().then((cart) => {
+        console.log(cart)
+        var items = cart.items
+        items.forEach(function (item, index) {
+          console.log(item.properties)
 
-
-            if(item.properties != null){
-
-              if(item.properties['_product_key']!= undefined){
-                itemsArray[item.variant_id] = item.properties['_product_key'];
-              }
-            }
-
-          });
-          console.log("check",itemsArray);
-          console.log(12334);
-
-          for(var index in itemsArray) {
-            console.log(index);
-            var checkItem = items.find(x => x.key === itemsArray[index]);
-            console.log('checkItem',checkItem);
-            if(checkItem == undefined){
-              removeItem(index);
+          if (item.properties != null) {
+            if (item.properties['_product_key'] != undefined) {
+              itemsArray[item.variant_id] = item.properties['_product_key']
             }
           }
-
         })
+        console.log('check', itemsArray)
+        console.log(12334)
+
+        for (var index in itemsArray) {
+          console.log(index)
+          var checkItem = items.find((x) => x.key === itemsArray[index])
+          console.log('checkItem', checkItem)
+          if (checkItem == undefined) {
+            removeItem(index)
+          }
+        }
       })
-      .catch(error => {
-        // handle the error
-      });
-
-
+    })
+    .catch((error) => {
+      // handle the error
+    })
 }
-function removeItem(id){
-  var updates = {};
-  updates[id] = 0;
-  jQuery.post(window.Shopify.routes.root + 'cart/update.js', {
-    updates: updates
-  }).done(function() {
-    location.reload();
-  });
-
+function removeItem(id) {
+  var updates = {}
+  updates[id] = 0
+  jQuery
+    .post(window.Shopify.routes.root + 'cart/update.js', {
+      updates: updates
+    })
+    .done(function () {
+      location.reload()
+    })
 }
